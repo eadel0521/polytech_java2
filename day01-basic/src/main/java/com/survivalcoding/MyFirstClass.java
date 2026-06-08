961d5e06b4d69c112a3514e69bee6a8d533f1821
InJoon.Lee <eadel0521@gmail.com>
2026-06-08 10:30:00 +0900

    java practice

---
+++ day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
@@ -0,0 +1,11 @@
+package com.survivalcoding;
+
+public class MyFirstClass {
+    public static void main(String[] args) {
+
+        int i = 0;
+        i = 10;
+
+        System.out.println("hello");
+    }
+}

0d7781b7666a327cb5ca0f083de8914793e43e9a
InJoon.Lee <eadel0521@gmail.com>
2026-06-22 16:56:48 +0900

    code style check and TIL

--- day04-abstract/src/main/java/com/survivalcoding/Asset.java
+++ day04-abstract/src/main/java/com/survivalcoding/Asset.java
@@ -12,27 +12,68 @@ class Patent extends IntangibleAsset {
 
 }
 
-abstract class TangibleAsset extends Asset implements Thing{
-    String name;
-    int price;
-    String color;
+abstract class TangibleAsset extends Asset implements Thing {
+    private String name;
+    private int price;
+    private String color;
     private double weight;
 
+    public TangibleAsset(String name, int price, String color, double weight) {
+        this.name = name;
+        this.price = price;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     @Override
-    public double getWeight(){
+    public double getWeight() {
         return weight;
     }
 
     @Override
-    public void setWeight(double weight){
+    public void setWeight(double weight) {
         this.weight = weight;
     }
 }
 
 class Book extends TangibleAsset {
-    String isbn;
+    private String isbn;
+
+    public Book(String name, int price, String color, double weight, String isbn) {
+        super(name, price, color, weight);
+        this.isbn = isbn;
+    }
 }
 
 class Computer extends TangibleAsset {
-    String makerName;
+    private String makerName;
+
+    public Computer(String name, int price, String color, double weight, String makerName) {
+        super(name, price, color, weight);
+        this.makerName = makerName;
+    }
 }
--- day04-abstract/src/test/java/com/survivalcoding/TangibleAssetTest.java
+++ day04-abstract/src/test/java/com/survivalcoding/TangibleAssetTest.java
@@ -1,14 +1,14 @@
 package com.survivalcoding;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TangibleAssetTest {
     @Test
-    void GetterSetterTest(){
-        Computer com = new Computer();
+    void GetterSetterTest() {
+        Computer com = new Computer("Gram", 20000, "white", 2.0, "LG");
         com.setWeight(2.5);
         assertEquals(2.5, com.getWeight());
     }
 }
-
